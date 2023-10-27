690dbebd2c4f160f0ed855fa91bc78493041f9e7
Beck Orion <beckorion@gmail.com>
2023-10-27 09:56:23 -0700

    added buy function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Imports
 import pygame # pygame is NEEDED for the file to run (it handles sound)
-print("Hello world")
+for _ in range(3):
+    print(f"/n/n/n") # 'clears' screen
 
 # Make some vars global
 global money
@@ -12,9 +13,25 @@ global points
 money = 50
 points = 10
 
+# Set list
+#// put a 'shopping cart' list here
+
 # Defs
 def is_valid_input(input_to_check):
-    print()
+    print("function not written yet")
+
+def ask_to_buy(item, cost, point_penalty):
+    choice = print(f"Do you want to buy {item} for ${cost}? ")
+    if choice == 'y':
+        money = money - cost
+        print(f"UPDATE: {item} added to cart")
+        # Add item to shopping cart list
+        # break?
+    else:
+        points = points - point_penalty
+        # Don't show the user point penalty, they have to infer
+        print(f"UPDATE: {item} skipped")
+
 
 def game_over(reason):
     for _ in range(10):
@@ -40,8 +57,10 @@ def update_money(subtraction):
 print("For now, just type y or n for each question")
 
 # Code/game
+
 # Cart
 if input("Do you want to get a shopping cart?: ") == 'y':
     pass
 else:
     game_over("You need a shopping cart to go shopping")
+
