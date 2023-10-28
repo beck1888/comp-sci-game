4167f13ee945fd86af2d38651eb62fef7dbf9c6a
Beck Orion <beckorion@gmail.com>
2023-10-28 02:54:29 -0700

    working credit module

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ def give_credit():
     print(
         '''CREDITS:
         Thank you to freesound.org for providing the sound clips used in this project.
+
+        > buy.wav [Creative Commons 0] ... https://freesound.org/people/CapsLok/sounds/184438/ 
+        > game_over.wav [Creative Commons 4.0] ... https://freesound.org/people/Doctor_Jekyll/sounds/240195/ (shortened in QuickTime)
+        > game_win.wav [Creative Commons 0] ... original from https://freesound.org/people/EVRetro/sounds/495005/
+        > police.wav [Creative Commons 0] ... https://freesound.org/people/guitarguy1985/sounds/70938/
         '''
     )
 
